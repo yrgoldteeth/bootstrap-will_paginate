f16047568415ec90e44af6e85f0568b70750dbf1
Nicholas Fine <nicholas.fine@gmail.com>
2011-12-22 15:01:09 -0600

    add gap method to bootstrap link renderer for large collections

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -21,6 +21,10 @@ module WillPaginate
         tag :li, link(page, page, :rel => rel_value(page)), :class => ('active' if page == current_page)
       end
 
+      def gap
+        tag :li, link(super, '#'), :class => 'disabled'
+      end
+
       def previous_or_next_page(page, text, classname)
         tag :li, link(text, page || '#'), :class => [classname[0..3], classname, ('disabled' unless page)].join(' ')
       end
