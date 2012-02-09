ab7c0cbac0ee087716ec960dd0f87fc50a54110d
Nicholas Fine <nicholas.fine@gmail.com>
2012-02-09 08:03:22 -0600

    add #infer_collection_from_controller to will_paginate helper

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -8,6 +8,9 @@ require 'will_paginate/view_helpers/action_view'
 module WillPaginate
   module ActionView
     def will_paginate(collection = nil, options = {})
+      options, collection = collection, nil if collection.is_a? Hash
+      # Taken from original will_paginate code to handle if the helper is not passed a collection object.
+      collection ||= infer_collection_from_controller 
       options[:renderer] ||= BootstrapLinkRenderer
       super.try :html_safe
     end
