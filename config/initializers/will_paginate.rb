cae74047cc19ba63dafbbdd78f953b8a6e281f86
Daniel Goh <gohdaniel15@gmail.com>
2018-04-23 14:53:39 +0800

    Add support for pagination for Bootstrap 2 users

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -72,5 +72,30 @@ module WillPaginate
          ["pagination", container_attributes[:class]].compact.join(" ")
       end
     end
+
+    class Bootstrap2LinkRenderer < LinkRenderer
+      protected
+
+      def html_container(html)
+        tag :div, tag(:ul, html), container_attributes
+      end
+
+      def page_number(page)
+        tag :li, link(page, page, :rel => rel_value(page)), :class => ('active' if page == current_page)
+      end
+
+      def gap
+        tag :li, link('&hellip;'.html_safe, '#'), :class => 'disabled'
+      end
+
+      def previous_or_next_page(page, text, classname)
+        if page
+          tag :li, link(text, page),
+            :class => [(classname[0..3] if  @options[:page_links]), (classname if @options[:page_links])].join(' ')
+        else
+          tag :li, tag(:span, text), :class => classname + ' disabled'
+        end
+      end
+    end
   end
 end
